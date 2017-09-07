28232553612287b64aaa70394820e3c514f38379
StevenOrola <stevenorola@gmail.com>
2017-09-07 13:54:32 -0700

    added comments

--- Student.php
+++ Student.php
@@ -1,18 +1,17 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Student
+ * Student class which has a first and last name,
+ * set of emails they use, grades and outputs
+ * this information in a formatted way
  *
- * @author steve
+ * @author Steven Orola
  */
 class Student {
    
+    /**
+     * Constructor for Student
+     */
     function _construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,23 +19,42 @@ class Student {
         $this->grades = array();
     }
     
+    /**
+     * Student can have as many emails as they need
+     * @param type $which home or work email
+     * @param type $address the address
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
     
+    /**
+     * Stores all grades, no courses are assigned
+     * @param type $grade a single grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /**
+     * Calculates all grades, rounded up
+     * to two decimals
+     * @return type
+     */
     function average() {
         $total = 0;
+        $average = 0;
         foreach ($this->grades as $value) {
             $total += $value;
         }
-        
-        return round($total / count($this->grades), 2);
+        $average = round($total / count($this->grades), 2);
+        return $average;
     }
     
+    /**
+     * formats Student's information in a readadble way
+     * @return result the Student's information
+     */
     function toString() {
         $result = "\t";
         $result .= $this->first_name . " " . $this->surname;
